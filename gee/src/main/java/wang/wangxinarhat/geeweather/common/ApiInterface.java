e8ec45d9c478cdeeca165d3d8a1394e0a0915a8b
wangxinarhat <wangxinarhat@gmail.com>
2016-03-31 21:54:37 +0800

    add rx

--- gee/src/main/java/wang/wangxinarhat/geeweather/common/ApiInterface.java
+++ gee/src/main/java/wang/wangxinarhat/geeweather/common/ApiInterface.java
@@ -1,8 +1,6 @@
 package wang.wangxinarhat.geeweather.common;
 
 
-import android.database.Observable;
-
 import retrofit.http.GET;
 import retrofit.http.Query;
 import wang.wangxinarhat.geeweather.domain.VersionAPI;
@@ -16,9 +14,10 @@ public interface ApiInterface {
     String HOST = "https://api.heweather.com/x3/";
 
     @GET("weather")
-    Observable<WeatherAPI> mWeatherAPI(@Query("city") String city, @Query("key") String key);
+    rx.Observable<WeatherAPI> mWeatherAPI(@Query("city") String city, @Query("key") String key);
 
     //而且在Retrofit 2.0中我们还可以在@Url里面定义完整的URL：这种情况下Base URL会被忽略。
-    @GET("http://api.fir.im/apps/latest/5630e5f1f2fc425c52000006") Observable<VersionAPI> mVersionAPI(
+    @GET("http://api.fir.im/apps/latest/5630e5f1f2fc425c52000006")
+    rx.Observable<VersionAPI> mVersionAPI(
             @Query("api_token") String api_token);
 }
--- gee/src/main/java/wang/wangxinarhat/geeweather/ui/MainActivity.java
+++ gee/src/main/java/wang/wangxinarhat/geeweather/ui/MainActivity.java
@@ -1,7 +1,12 @@
 package wang.wangxinarhat.geeweather.ui;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -9,41 +14,199 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import rx.Observable;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 import wang.wangxinarhat.geeweather.R;
+import wang.wangxinarhat.geeweather.adapter.WeatherAdapter;
+import wang.wangxinarhat.geeweather.common.RetrofitSingleton;
 import wang.wangxinarhat.geeweather.domain.Setting;
+import wang.wangxinarhat.geeweather.domain.Weather;
+import wang.wangxinarhat.geeweather.domain.WeatherAPI;
+import wang.wangxinarhat.geeweather.utils.SomeUtils;
 
 public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
+    @Bind(R.id.bannner)
+    ImageView bannner;
     @Bind(R.id.toolbar)
     Toolbar toolbar;
-
-    @Bind(R.id.app_bar)
+    @Bind(R.id.collapsingtoolbar)
+    CollapsingToolbarLayout collapsingtoolbar;
+    @Bind(R.id.appbar)
     AppBarLayout appbar;
+    @Bind(R.id.recycler)
+    RecyclerView recycler;
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
+
+
     @Bind(R.id.swipe)
-    SwipeRefreshLayout swipeRefreshLayout;
+    SwipeRefreshLayout swipe;
     @Bind(R.id.fab)
     FloatingActionButton fab;
-    @Bind(R.id.drawer_layout)
+    @Bind(R.id.coordinator)
+    CoordinatorLayout coordinator;
+    @Bind(R.id.navigation)
+    NavigationView navigation;
+    @Bind(R.id.drawer)
     DrawerLayout drawer;
-    @Bind(R.id.nav_view)
-    NavigationView navigationView;
+
+    private static final String TAG = "MainActivity";
+    private Observer<Weather> observer;
+    private WeatherAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
         initIcon();
         initView();
+
+        fetchData();
     }
+
+    /**
+     * <p/>
+     * 首先从本地缓存获取数据
+     * if 有
+     * 更新UI
+     * else
+     * 直接进行网络请求，更新UI并保存在本地
+     */
+    private void fetchData() {
+        observer = new Observer<Weather>() {
+            @Override
+            public void onCompleted() {
+                new RefreshHandler().sendEmptyMessage(2);
+            }
+
+
+            @Override
+            public void onError(Throwable e) {
+                RetrofitSingleton.disposeFailureInfo(e, MainActivity.this, fab);
+                new RefreshHandler().sendEmptyMessage(2);
+            }
+
+            @Override
+            public void onNext(Weather weather) {
+                progressBar.setVisibility(View.INVISIBLE);
+                new RefreshHandler().sendEmptyMessage(2);
+                collapsingtoolbar.setTitle(weather.basic.city);
+                mAdapter = new WeatherAdapter(MainActivity.this, weather);
+                recycler.setAdapter(mAdapter);
+            }
+        };
+
+        fetchDataByCache(observer);
+    }
+
+
+    /**
+     * 从本地获取
+     */
+    private void fetchDataByCache(Observer<Weather> observer) {
+
+        Weather weather = null;
+        try {
+            weather = (Weather) aCache.getAsObject("WeatherData");
+        } catch (Exception e) {
+            Log.e(TAG, e.toString());
+        }
+
+        if (weather != null) {
+            Observable.just(weather).distinct().subscribe(observer);
+        } else {
+            fetchDataByNetWork(observer);
+        }
+    }
+
+    /**
+     * 从网络获取
+     */
+    private void fetchDataByNetWork(Observer<Weather> observer) {
+        String cityName = mSetting.getString(Setting.CITY_NAME, "北京");
+        if (cityName != null) {
+            cityName = cityName.replace("市", "")
+                    .replace("省", "")
+                    .replace("自治区", "")
+                    .replace("特别行政区", "")
+                    .replace("地区", "")
+                    .replace("盟", "");
+
+
+        }
+
+
+        RetrofitSingleton.getApiService(this)
+                .mWeatherAPI(cityName, Setting.KEY)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .filter(new Func1<WeatherAPI, Boolean>() {
+                    @Override
+                    public Boolean call(WeatherAPI weatherAPI) {
+                        return weatherAPI.mHeWeatherDataService30s.get(0).status.equals("ok");
+                    }
+                })
+                .map(new Func1<WeatherAPI, Weather>() {
+                    @Override
+                    public Weather call(WeatherAPI weatherAPI) {
+                        return weatherAPI.mHeWeatherDataService30s.get(0);
+                    }
+                })
+                .doOnNext(new Action1<Weather>() {
+                    @Override
+                    public void call(Weather weather) {
+                        aCache.put("WeatherData", weather,
+                                (mSetting.getInt(Setting.AUTO_UPDATE, 0) + 1) * Setting.ONE_HOUR);//默认一小时后缓存失效
+                    }
+                })
+                .subscribe(observer);
+    }
+
+
+    @SuppressLint("HandlerLeak")
+    class RefreshHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    swipe.setRefreshing(true);
+                    break;
+                case 2:
+                    if (swipe.isRefreshing()) {
+                        swipe.setRefreshing(false);
+
+                        if (SomeUtils.isNetworkConnected(MainActivity.this)) {
+                            Snackbar.make(fab, "加载完毕，✺◟(∗❛ัᴗ❛ั∗)◞✺", Snackbar.LENGTH_SHORT).show();
+                        } else {
+                            Snackbar.make(fab, "网络出了些问题？( ´△｀)", Snackbar.LENGTH_SHORT).show();
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
 
     private void initView() {
 
@@ -53,8 +216,29 @@ public class MainActivity extends BaseActivity
 
     private void initDesignwidget() {
 
+//        bar
         setSupportActionBar(toolbar);
+//        toolbar.setLogo(R.mipmap.ic_launcher);
+        toolbar.setTitle("My Title");
+        toolbar.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        toolbar.setSubtitle("Sub title");
 
+
+//        swipe
+        appbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+
+                if (verticalOffset >= 0) {
+                    swipe.setEnabled(true);
+                } else {
+                    swipe.setEnabled(false);
+                }
+            }
+        });
+
+
+//        fab
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -63,84 +247,17 @@ public class MainActivity extends BaseActivity
             }
         });
 
+
+//        drawer
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
-
-
-        // App Logo
-//        toolbar.setLogo(R.mipmap.ic_launcher);
-        // Title
-        toolbar.setTitle("My Title");
-        toolbar.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        // Sub Title
-        toolbar.setSubtitle("Sub title");
-
-
-        appbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
-            @Override
-            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-
-                if (verticalOffset >= 0) {
-                    swipeRefreshLayout.setEnabled(true);
-                } else {
-                    swipeRefreshLayout.setEnabled(false);
-                }
-            }
-        });
+        navigation.setNavigationItemSelectedListener(this);
 
 
     }
 
-
-    /**
-     * 初始化Icon
-     */
-    private void initIcon() {
-        if (mSetting.getInt(Setting.CHANGE_ICONS, 0) == 0) {
-            mSetting.putInt("未知", R.mipmap.none);
-            mSetting.putInt("晴", R.mipmap.type_one_sunny);
-            mSetting.putInt("阴", R.mipmap.type_one_cloudy);
-            mSetting.putInt("多云", R.mipmap.type_one_cloudy);
-            mSetting.putInt("少云", R.mipmap.type_one_cloudy);
-            mSetting.putInt("晴间多云", R.mipmap.type_one_cloudytosunny);
-            mSetting.putInt("小雨", R.mipmap.type_one_light_rain);
-            mSetting.putInt("中雨", R.mipmap.type_one_middle_rain);
-            mSetting.putInt("大雨", R.mipmap.type_one_heavy_rain);
-            mSetting.putInt("阵雨", R.mipmap.type_one_thunderstorm);
-            mSetting.putInt("雷阵雨", R.mipmap.type_one_thunderstorm);
-            mSetting.putInt("霾", R.mipmap.type_one_fog);
-            mSetting.putInt("雾", R.mipmap.type_one_fog);
-        } else {
-            mSetting.putInt("未知", R.mipmap.none);
-            mSetting.putInt("晴", R.mipmap.type_two_sunny);
-            mSetting.putInt("阴", R.mipmap.type_two_cloudy);
-            mSetting.putInt("多云", R.mipmap.type_two_cloudy);
-            mSetting.putInt("少云", R.mipmap.type_two_cloudy);
-            mSetting.putInt("晴间多云", R.mipmap.type_two_cloudytosunny);
-            mSetting.putInt("小雨", R.mipmap.type_two_light_rain);
-            mSetting.putInt("中雨", R.mipmap.type_two_rain);
-            mSetting.putInt("大雨", R.mipmap.type_two_rain);
-            mSetting.putInt("阵雨", R.mipmap.type_two_rain);
-            mSetting.putInt("雷阵雨", R.mipmap.type_two_thunderstorm);
-            mSetting.putInt("霾", R.mipmap.type_two_haze);
-            mSetting.putInt("雾", R.mipmap.type_two_fog);
-            mSetting.putInt("雨夹雪", R.mipmap.type_two_snowrain);
-        }
-    }
-
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -184,8 +301,56 @@ public class MainActivity extends BaseActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /**
+     * 初始化Icon
+     */
+    private void initIcon() {
+        if (mSetting.getInt(Setting.CHANGE_ICONS, 0) == 0) {
+            mSetting.putInt("未知", R.mipmap.none);
+            mSetting.putInt("晴", R.mipmap.type_one_sunny);
+            mSetting.putInt("阴", R.mipmap.type_one_cloudy);
+            mSetting.putInt("多云", R.mipmap.type_one_cloudy);
+            mSetting.putInt("少云", R.mipmap.type_one_cloudy);
+            mSetting.putInt("晴间多云", R.mipmap.type_one_cloudytosunny);
+            mSetting.putInt("小雨", R.mipmap.type_one_light_rain);
+            mSetting.putInt("中雨", R.mipmap.type_one_middle_rain);
+            mSetting.putInt("大雨", R.mipmap.type_one_heavy_rain);
+            mSetting.putInt("阵雨", R.mipmap.type_one_thunderstorm);
+            mSetting.putInt("雷阵雨", R.mipmap.type_one_thunderstorm);
+            mSetting.putInt("霾", R.mipmap.type_one_fog);
+            mSetting.putInt("雾", R.mipmap.type_one_fog);
+        } else {
+            mSetting.putInt("未知", R.mipmap.none);
+            mSetting.putInt("晴", R.mipmap.type_two_sunny);
+            mSetting.putInt("阴", R.mipmap.type_two_cloudy);
+            mSetting.putInt("多云", R.mipmap.type_two_cloudy);
+            mSetting.putInt("少云", R.mipmap.type_two_cloudy);
+            mSetting.putInt("晴间多云", R.mipmap.type_two_cloudytosunny);
+            mSetting.putInt("小雨", R.mipmap.type_two_light_rain);
+            mSetting.putInt("中雨", R.mipmap.type_two_rain);
+            mSetting.putInt("大雨", R.mipmap.type_two_rain);
+            mSetting.putInt("阵雨", R.mipmap.type_two_rain);
+            mSetting.putInt("雷阵雨", R.mipmap.type_two_thunderstorm);
+            mSetting.putInt("霾", R.mipmap.type_two_haze);
+            mSetting.putInt("雾", R.mipmap.type_two_fog);
+            mSetting.putInt("雨夹雪", R.mipmap.type_two_snowrain);
+        }
+    }
+
 }
